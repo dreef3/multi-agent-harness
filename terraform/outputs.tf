14111e6be07f5c71f7c02a2727ab6ef281f9444b
Andrey Ermakov <dreef3@gmail.com>
2026-04-20 20:39:14 +0000

    chore: remove terraform (migrated to dreef3/dotfiles)

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP of the OCI VM (for emergency SSH access)"
-  value       = oci_core_instance.harness.public_ip
-}
-
-output "instance_id" {
-  description = "OCID of the compute instance"
-  value       = oci_core_instance.harness.id
-}
